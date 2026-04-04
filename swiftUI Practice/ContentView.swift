b11d926836373a6978d0b8fd5e1a3021292fc57b
zhangyida-lab <79899307+zhangyida-lab@users.noreply.github.com>
2026-04-04 22:20:18 +0800

    Initial Commit

---
+++ swiftUI Practice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftUI Practice
+//
+//  Created by tony on 4/4/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftUI Practice/swiftUI_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_PracticeApp.swift
+//  swiftUI Practice
+//
+//  Created by tony on 4/4/26.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
