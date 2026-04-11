5fe7c2743e29eb50e9bdfd35523aa59d5d320508
zhangyida-lab <79899307+zhangyida-lab@users.noreply.github.com>
2026-04-11 08:33:15 +0800

    wip: 完成xcode配置

---
+++ QuizappTests/QuizappTests.swift
@@ -0,0 +1,16 @@
+//
+//  QuizappTests.swift
+//  QuizappTests
+//
+//  Created by tony on 4/8/26.
+//
+
+import Testing
+
+struct QuizappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
